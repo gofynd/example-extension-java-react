9ef526dfa975218d36c7c942966f59f05e8af227
Bhargav Prajapati <bhargavprajapati@gofynd.com>
2022-12-29 13:59:11 +0530

    Changes after code-review from Parimal Patel

--- src/test/java/com/fynd/example/java/controller/RouteControllerTest.java
+++ src/test/java/com/fynd/example/java/controller/RouteControllerTest.java
@@ -1,30 +1,14 @@
 package com.fynd.example.java.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
+
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
-
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//class DefaultControllerTest {
-//
-//    @LocalServerPort
-//    private int port;
-//    @Mock
-//    private RestTemplate restTemplate;
-//
-//    @Test
-//    void redirect() {
-//        ResultMatcher res = restTemplate.getForObject("http://localhost:"+ port + "/company/1", ResultMatcher.class);
-//        System.out.println(res);
-//        assertThat(res).isEqualTo(forwardedUrl("/"));
-//    }
-//}
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(RouteController.class)
