1ebc578907ad7e90c37361c9185015a7655b9a22
aayush jain <aayushvjain@gofynd.com>
2023-01-03 10:54:35 +0530

    example extension react java

---
+++ src/test/java/com/fynd/example/java/ExampleJavaApplicationTests.java
@@ -0,0 +1,14 @@
+package com.fynd.example.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+
+}
