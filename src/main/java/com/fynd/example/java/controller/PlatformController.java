76e4ad704ce75de923986bee888803a5c1d91f13
Manish Prajapati <manishprajapati@gofynd.com>
2024-10-10 13:41:49 +0530

    ID: FPCO-24112; Update arguments of updated getProductsApp method

--- src/main/java/com/fynd/example/java/controller/PlatformController.java
+++ src/main/java/com/fynd/example/java/controller/PlatformController.java
@@ -22,7 +22,7 @@ public class PlatformController extends BasePlatformController {
     public CatalogPlatformModels.ProductListingResponseV2 getProducts(HttpServletRequest request) {
         try {
             PlatformClient platformClient = (PlatformClient) request.getAttribute("platformClient");
-            return platformClient.catalog.getProducts(Collections.emptyList(), Collections.emptyList(), Collections.emptyList(), Collections.emptyList(), Collections.emptyList(), "", "", Collections.emptyList(), "", Collections.emptyList(), 1, 10, "", "", "");
+            return platformClient.catalog.getProducts(Collections.emptyList(), Collections.emptyList(), Collections.emptyList(), Collections.emptyList(), Collections.emptyList(), "", "", Collections.emptyList(), "", Collections.emptyList(), 1, 10, "number", "latest", "");
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
@@ -34,7 +34,7 @@ public class PlatformController extends BasePlatformController {
     public CatalogPlatformModels.RawProductListingResponse getAppProducts(@PathVariable("application_id") String applicationId, HttpServletRequest request) {
         try {
             PlatformClient platformClient = (PlatformClient) request.getAttribute("platformClient");
-            return platformClient.application(applicationId).catalog.getAppProducts(Collections.emptyList(), Collections.emptyList(), Collections.emptyList(), Collections.emptyList(), Collections.emptyList(), "", "", Collections.emptyList(), "", Collections.emptyList(), 1, 10, "number", "latest", "");
+            return platformClient.application(applicationId).catalog.getAppProducts(Collections.emptyList(),Collections.emptyList(), Collections.emptyList(), Collections.emptyList(), Collections.emptyList(), 1, 10, "");
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
